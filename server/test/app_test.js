04df1880b1d8ef00ceeea35b33c4f63bc7faaf3b
Kazuyuki Honda <hakobera@gmail.com>
2014-05-25 12:02:57 +0900

    Add /shared/dashboards test

--- server/test/app_test.js
+++ server/test/app_test.js
@@ -1,6 +1,9 @@
 var request = require('supertest');
-var app = require('../src/app');
 var influx = require('influx');
+var async = require('async');
+var fs = require('fs');
+
+var app = require('../src/app');
 
 var config = {
   "dashboardDbPath": "./test.db",
@@ -8,7 +11,8 @@ var config = {
   "port": 8086,
   "database": "test",
   "username": "root",
-  "password": "root"
+  "password": "root",
+  "useProxy": true
 };
 
 var server = app.create(config);
@@ -23,11 +27,93 @@ describe('app', function () {
     client.deleteDatabase(config.database, done);
   });
 
-  describe('#ping', function () {
-    it('should success', function (done) {
+  describe('GET /config.json', function () {
+    it('should return influxdb config', function (done) {
       request(server)
-        .get('/')
+        .get('/config.json')
+        .expect('Content-Type', /json/)
+        .expect(200, { database: 'test', useProxy: true })
         .end(done);
+    });
+  });
+
+  describe('/shared/dashboards', function () {
+    afterEach(function () {
+      if (fs.existsSync(config.dashboardDbPath)) {
+        fs.unlinkSync(config.dashboardDbPath);
+      }
+    });
+
+    it('should return empty array if now dashboards', function (done) {
+      request(server)
+        .get('/shared/dashboards')
+        .expect('Content-Type', /json/)
+        .expect(200, [])
+        .end(done);
+    });
+
+    it('should return dashboards', function (done) {
+      var dashboard1 = require('../../app/assets/dashboards/default.json');
+      var dashboard2 = require('../../app/assets/dashboards/test.json');
+
+      async.series([
+        function (next) {
+          // Return emtpy array if no dashboards
+          request(server)
+            .get('/shared/dashboards')
+            .expect('Content-Type', /json/)
+            .expect(200, [])
+            .end(next);
+        },
+        function (next) {
+          request(server)
+            .post('/shared/dashboards')
+            .send(dashboard1)
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .expect(function (res) {
+              if (!('_id' in res.body)) return 'missing _id';
+              if (res.body.type !== 'dashboard') return 'type must be dashboard';
+              if (res.body.name !== dashboard1.name) return 'name is wrong';
+            })
+            .end(next);
+        },
+        function (next) {
+          request(server)
+            .get('/shared/dashboards')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .expect(function (res) {
+              var dashboards = res.body;
+              if (dashboards.length !== 1) return 'dashboards.length expects 1 but ' + dashboards.length;
+            })
+            .end(next);
+        },
+        function (next) {
+          request(server)
+            .post('/shared/dashboards')
+            .send(dashboard2)
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .expect(function (res) {
+              if (!('_id' in res.body)) return 'missing _id';
+              if (res.body.type !== 'dashboard') return 'type must be dashboard';
+              if (res.body.name !== dashboard2.name) return 'name is wrong';
+            })
+            .end(next);
+        }
+      ], function (err) {
+        if (err) return done(err);
+        request(server)
+          .get('/shared/dashboards')
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .expect(function (res) {
+            var dashboards = res.body;
+            if (dashboards.length !== 2) return 'dashboards.length expects 2 but ' + dashboards.length;
+          })
+          .end(done);
+      });
     });
   });
 });
